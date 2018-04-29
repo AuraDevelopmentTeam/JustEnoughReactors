63117981ce03d7656aa542817ee0d5b7e4f3520e
BrainStone <yannick@tedworld.de>
2018-04-30 01:53:28 +0200

    Corrected rendering

--- src/main/java/dev/aura/justenoughreactors/util/DrawableFrame.java
+++ src/main/java/dev/aura/justenoughreactors/util/DrawableFrame.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import mezz.jei.api.gui.IDrawable;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 
 @RequiredArgsConstructor
@@ -18,8 +19,16 @@ public class DrawableFrame implements IDrawable {
   private final int paddingLeft;
 
   private static void drawTexturedRectangle(int x, int y, float rotation) {
+    final int halfSize = renderSize / 2;
+
+    GlStateManager.pushMatrix();
+    GlStateManager.translate(x + halfSize, y + halfSize, 0);
+    GlStateManager.rotate(rotation, 0, 0, 1);
+
     Gui.drawModalRectWithCustomSizedTexture(
-        x, y, 0, 0, renderSize, renderSize, renderSize, renderSize);
+        -halfSize, -halfSize, 0, 0, renderSize, renderSize, renderSize, renderSize);
+
+    GlStateManager.popMatrix();
   }
 
   public DrawableFrame(ResourceLocation corner, ResourceLocation side, ResourceLocation center) {
@@ -51,10 +60,10 @@ public class DrawableFrame implements IDrawable {
     drawTexturedRectangle(x + shift2, y + shift2, 270);
 
     minecraft.getTextureManager().bindTexture(side);
-    drawTexturedRectangle(x, y + shift1, 0);
-    drawTexturedRectangle(x + shift1, y, 90);
-    drawTexturedRectangle(x + shift2, y + shift1, 180);
-    drawTexturedRectangle(x + shift1, y + shift2, 270);
+    drawTexturedRectangle(x + shift1, y, 0);
+    drawTexturedRectangle(x, y + shift1, 90);
+    drawTexturedRectangle(x + shift1, y + shift2, 180);
+    drawTexturedRectangle(x + shift2, y + shift1, 270);
 
     minecraft.getTextureManager().bindTexture(center);
     drawTexturedRectangle(x + shift1, y + shift1, 0);
